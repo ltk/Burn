18e3de12e67e35a8a6f1fec75a8341ded918429c
Lawson Kurtz <lawson.kurtz@viget.com>
2013-10-06 17:29:02 -0400

    remove debugging

--- app/controllers/inbound_messages_controller.rb
+++ app/controllers/inbound_messages_controller.rb
@@ -7,12 +7,6 @@ class InboundMessagesController < ApplicationController
   # ToDo: setup auth token for webhook URL
 
   def create
-    puts "Create"
-    puts inbound_messages
-    inbound_messages.each do |msg|
-      debug msg
-    end
-
     inbound_messages.each(&:save)
     render :nothing => true, :status => 200
   end
@@ -24,21 +18,16 @@ class InboundMessagesController < ApplicationController
   end
 
   def mandrill_events
-    puts "Mandrill Events"
     JSON.parse(params[:mandrill_events]) || []
   end
 
   def inbound_mandrill_events
-    puts "Inbound Mandrill Events"
-    puts mandrill_events
     mandrill_events.select do |event|
       event[:event] == 'inbound'
     end
   end
 
   def inbound_messages
-    puts "Inbound Messages"
-    puts inbound_mandrill_events
     @inbound_messages ||= inbound_mandrill_events.map do |event|
       InboundMessage.from_mandrill_event(event)
     end
--- app/models/inbound_message.rb
+++ app/models/inbound_message.rb
@@ -15,19 +15,19 @@ class InboundMessage < ActiveRecord::Base
 
 
   def self.from_mandrill_event(e)
-    new :raw        => e[:msg][:raw_msg],
-        :headers    => e[:msg][:headers],
-        :text       => e[:msg][:text],
-        :html       => e[:msg][:html],
-        :from_email => e[:msg][:from_email],
-        :from_name  => e[:msg][:from_name],
-        :to         => e[:msg][:to],
-        :email      => e[:msg][:email],
-        :subject    => e[:msg][:subject],
-        :spam_score => e[:msg][:spam_report][:score],
-        :spf_result => e[:msg][:spf][:result],
-        :dkim_valid => e[:msg][:dkim][:valid],
-        :burner     => Burner.find_by_slug(slug_from_email_address(e[:msg][:email]))
+    new :raw        => e['msg']['raw_msg'],
+        :headers    => e['msg']['headers'],
+        :text       => e['msg']['text'],
+        :html       => e['msg']['html'],
+        :from_email => e['msg']['from_email'],
+        :from_name  => e['msg']['from_name'],
+        :to         => e['msg']['to'],
+        :email      => e['msg']['email'],
+        :subject    => e['msg']['subject'],
+        :spam_score => e['msg']['spam_report']['score'],
+        :spf_result => e['msg']['spf']['result'],
+        :dkim_valid => e['msg']['dkim']['valid'],
+        :burner     => Burner.find_by_slug(slug_from_email_address(e['msg']['email']))
   end
 
   def self.slug_from_email_address(address)
