d38a763022b32dfa04de877d07a0f03476a1d5f3
Lawson Kurtz <lawson.kurtz@viget.com>
2013-10-06 17:19:18 -0400

    debug

--- app/controllers/inbound_messages_controller.rb
+++ app/controllers/inbound_messages_controller.rb
@@ -7,6 +7,7 @@ class InboundMessagesController < ApplicationController
   # ToDo: setup auth token for webhook URL
 
   def create
+    puts "Create"
     inbound_messages.each do |msg|
       debug msg
     end
@@ -22,16 +23,19 @@ class InboundMessagesController < ApplicationController
   end
 
   def mandrill_events
+    puts "Mandrill Events"
     JSON.parse(params[:mandrill_events]) || []
   end
 
   def inbound_mandrill_events
+    puts "Inbound Mandrill Events"
     mandrill_events.select do |event|
       event[:event] == 'inbound'
     end
   end
 
   def inbound_messages
+    puts "Inbound Messages"
     @inbound_messages ||= inbound_mandrill_events.map do |event|
       InboundMessage.from_mandrill_event(event)
     end
